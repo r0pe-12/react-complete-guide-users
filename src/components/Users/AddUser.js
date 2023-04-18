0c5cae3413553f2708d1a68a45045d64c0ee9757
Petar Simonovic <simonovicp12@gmail.com>
2023-04-18 02:31:39 +0200

    added Card component and styled AddUser component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,17 +1,22 @@
+import styles from './AddUser.module.css'
+import Card from "../UI/Card";
+
 const AddUser = () => {
 
     const addUserHandler = (event) => {
-      event.preventDefault();
+        event.preventDefault();
     }
 
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input id={"username"} type={"text"}/>
-            <label htmlFor="age">Age (Years)</label>
-            <input id={"age"} type="number"/>
-            <button type={"submit"}>Add User</button>
-        </form>
+        <Card className={styles.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id={"username"} type={"text"}/>
+                <label htmlFor="age">Age (Years)</label>
+                <input id={"age"} type="number"/>
+                <button type={"submit"}>Add User</button>
+            </form>
+        </Card>
     );
 }
 
