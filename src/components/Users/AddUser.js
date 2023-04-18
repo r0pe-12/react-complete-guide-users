ebef751d391ee4ededa0354812c1ed3d62d4c4e8
Petar Simonovic <simonovicp12@gmail.com>
2023-04-18 02:20:28 +0200

    added create user component

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,18 @@
+const AddUser = () => {
+
+    const addUserHandler = (event) => {
+      event.preventDefault();
+    }
+
+    return (
+        <form onSubmit={addUserHandler}>
+            <label htmlFor="username">Username</label>
+            <input id={"username"} type={"text"}/>
+            <label htmlFor="age">Age (Years)</label>
+            <input id={"age"} type="number"/>
+            <button type={"submit"}>Add User</button>
+        </form>
+    );
+}
+
+export default AddUser;
